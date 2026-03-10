b987ccee44473a6cabccc4417fa85a2816242077
Bram Vandamme-Marteel <bramvandammemarteel@gmail.com>
2026-03-10 18:23:18 +0100

    dump 10/3/2026

--- output/engine2_dll.rs
+++ output/engine2_dll.rs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-03-04 23:03:20.863763100 UTC
+// 2026-03-10 17:23:00.390854500 UTC
 
 #![allow(non_upper_case_globals, non_camel_case_types, non_snake_case, unused)]
 
