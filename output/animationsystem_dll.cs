dfe6ed35c6070c2133fa2797d4a838427eaa3755
Bram Vandamme-Marteel <bram@skkills.com>
2026-04-03 09:33:09 +0200

    3/4

--- output/animationsystem_dll.cs
+++ output/animationsystem_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-04-02 16:16:12.481709900 UTC
+// 2026-04-03 07:32:21.927170700 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: animationsystem.dll
