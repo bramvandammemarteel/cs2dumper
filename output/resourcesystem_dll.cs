3c67a8fb90d7e16600082d8e0eeb2b6e408b1f73
Bram Vandamme-Marteel <bramvandammemarteel@gmail.com>
2026-04-07 16:50:38 +0200

    7/4

--- output/resourcesystem_dll.cs
+++ output/resourcesystem_dll.cs
@@ -1,9 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-<<<<<<< HEAD
-// 2026-04-07 14:48:01.803392400 UTC
-=======
-// 2026-04-03 07:32:21.927170700 UTC
->>>>>>> dfe6ed35c6070c2133fa2797d4a838427eaa3755
+// 2026-04-07 14:50:25.433255100 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: resourcesystem.dll
