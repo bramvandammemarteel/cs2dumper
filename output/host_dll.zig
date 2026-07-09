06ca6d0c280fd5ecfd502ad49755bbcc097d3008
Bram Vandamme-Marteel <bramvandammemarteel@gmail.com>
2026-07-09 09:58:27 +0200

    9/07/26

--- output/host_dll.zig
+++ output/host_dll.zig
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-06-10 22:21:18.446572300 UTC
+// 2026-07-09 07:57:36.555845200 UTC
 
 pub const cs2_dumper = struct {
     pub const schemas = struct {
